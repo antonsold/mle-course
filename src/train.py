c018afa0ecc22d648a47f0a3ce5ad27254c3d3d7
antonsold <soldatenkovanton@mail.ru>
2023-01-12 01:59:05 +0300

    added comments

--- src/train.py
+++ src/train.py
@@ -20,6 +20,8 @@ class Model:
         self.config = configparser.ConfigParser()
         self.log = logger.get_logger(__name__)
         self.config.read("config.ini")
+
+        # Loading files from preprocess stage
         self.X_train = pd.read_csv(
             self.config["SPLIT_DATA"]["X_train"], index_col=0)
         self.y_train = pd.read_csv(
@@ -28,13 +30,20 @@ class Model:
             self.config["SPLIT_DATA"]["X_test"], index_col=0)
         self.y_test = pd.read_csv(
             self.config["SPLIT_DATA"]["y_test"], index_col=0)
+
+        # Path to experiment results
         self.project_path = os.path.join(os.getcwd(), "experiments")
+
+        # Paths to models
         self.log_reg_path = os.path.join(self.project_path, "log_reg.sav")
         self.scaler_path = os.path.join(
             self.project_path, "scaler.sav")
         self.log.info("Model is ready")
 
     def scale(self):
+        """
+        Training and applying standard scaling
+        """
         sc = StandardScaler()
         self.X_train = sc.fit_transform(self.X_train)
         self.X_test = sc.transform(self.X_test)
@@ -42,12 +51,17 @@ class Model:
         return self.save_model(sc, self.scaler_path, 'SCALER', params)
 
     def log_reg(self, predict=False) -> bool:
+        """
+        Fits logistic regression and (optionally) makes predictions and prints accuracy
+        """
         classifier = LogisticRegression(solver='liblinear')
         try:
             classifier.fit(self.X_train, self.y_train['Outcome'])
         except Exception:
             self.log.error(traceback.format_exc())
             sys.exit(1)
+
+        # Making predictions if specified
         if predict:
             y_pred = classifier.predict(self.X_test)
             print(f"Accuracy: {accuracy_score(self.y_test, y_pred)}")
@@ -55,6 +69,9 @@ class Model:
         return self.save_model(classifier, self.log_reg_path, "LOG_REG", params)
 
     def save_model(self, classifier, path: str, name: str, params: dict) -> bool:
+        """
+        Saves model and writes path to config
+        """
         self.config[name] = params
         os.remove('config.ini')
         with open('config.ini', 'w') as configfile:
